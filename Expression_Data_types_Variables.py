e68babf9e9b9cd0c31f0a1518729b25851d8882d
sayes121 <61692463+sayes121@users.noreply.github.com>
2021-02-16 11:53:43 +1000

    Basic python concepts

---
+++ Expression_Data_types_Variables.py
@@ -0,0 +1,58 @@
+# EXPRESSION
+# An expression consist of values and operators
+print(2 + 2)
+print(3 * 5)
+print(4 - 2)
+print(6 / 3)
+
+# Order of operation
+# Multiplication, Division, Addition and Subtraction
+print((2 + 3) * 6)
+
+print(2 + 3 * 6)
+print((5-1) * ((7 + 1) / (3 - 1)))
+print('\n\n')
+
+# DATA TYPES
+# For whole numbers, positive or negative - Integer
+print(-2)
+print(30)
+
+# For decimal values - Floats
+print(3.14)
+print(type(3.14))
+
+# For text - Strings
+print('Hello World')
+
+# Concatenation in string
+print('Sayee' + 'sudha')
+
+# Expressions can also be used for string statements
+print('QUT' * 3)
+print('Hello' + '!' * 10)
+print('\n\n')
+
+# VARIABLES
+# It is name given by user which holds a value.
+# This value can be a string, int, or float
+# when the variable is called,
+# it's value will be printed as output
+x = 10
+print(x)
+
+# The variable data can be changed, by over-riding
+x = 'Hello'
+print(x + ' World')
+
+# Assignment statement
+# expression can be given be variable
+# This is an expression
+# As it gives a single value as output
+x = 2 + 2
+print(x)
+
+# This is a statement
+# as it will not give any output
+x = 6
+x = x + 1
---
+++ Writing_the_first_program.py
@@ -0,0 +1,14 @@
+# WRITING THE FIRST PROGRAM
+# Simple python programs are written
+# and executed
+
+# Hello.py
+print('Greetings')
+print('Your name please?')
+name = input()
+print('Nice meeting you ' + name)
+print('Your name length is', + (len(name)))
+print('What is your age?')
+age = input()
+print('Wow, you will be ' + str(int(age) + 1) + ' by next year')
+
